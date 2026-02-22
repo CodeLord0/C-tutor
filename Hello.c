7272db5364cae019c2c0e9474f8357cd677d6ec7
Tristan Emeana <tristan.emeana@gmail.com>
2026-02-22 17:47:08 +0000

    Add Hello.c with a simple greeting program

--- Hello.c
+++ Hello.c
@@ -5,7 +5,7 @@
 int main()
 {
 
-    char characterName[] = "Tristan"; //variable to store characters;
+    char characterName[] = "steve"; //variable to store characters;
     int characterAge = 35;// int variable
 
     printf("hello, my name is %s\n", characterName); // %s is the place holder for name
