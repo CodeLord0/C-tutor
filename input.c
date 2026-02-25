625a94ea744acb8ab8368d78ceefeb1a62eddc71
Tristan Emeana <tristan.emeana@gmail.com>
2026-02-25 02:11:56 +0000

    more on input

--- input.c
+++ input.c
@@ -6,13 +6,30 @@ int main()
 
     // int age;
     // printf("Enter your age: ");
-    // scanf("%d", &age);
+    // scanf("%d", &age);// &age is the pointer to the variable ag
     // printf("You are %d years old\n",age);
 
-    double gpa;
-    printf("Enter your gpa: ");
-    scanf("%lf", &gpa);
-    printf("You are %f years old\n",gpa);
+    // double gpa;
+    // printf("Enter your gpa: ");
+    // scanf("%lf", &gpa); // %lf is used for double data type when using scanf, and we need to use &gpa to store the input value in the variable gpa
+    // printf("You are %f years old\n",gpa);
+
+    // char grade;
+    // printf("Enter your grade: ");
+    // scanf("%c", &grade); // %c is used for chars
+    // printf("Your grade is %c", grade);
+
+    // char name[20]; //tells c number of characters than can be stored to allocate memory
+    // printf("enter your name: ");
+    // scanf("%s", name); //exits after the first space
+    // printf("your name is %s", name);
+
+    char name[20]; //tells c number of characters than can be stored to allocate memory
+    printf("enter your name: ");
+    fgets(name,20,stdin); // gets an entire line of string
+    printf("your name is %s", name);
+    printf("jaime");
+    
 
     return 0;
 }
